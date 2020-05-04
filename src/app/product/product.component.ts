06ad9f0450bf10d9d4e6ae591077b5dc0cd9b186
Aksh2209 <akshayap672@gmail.com>
2020-05-04 18:52:23 +0530

    2 files modified

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -4,4 +4,50 @@ import { Component } from '@angular/core';
   selector: 'product-section',
   templateUrl: 'product.component.html'
 })
-export class ProductSection {   }
+export class ProductSection { 
+  productInfo = {
+    "message":"APIPACK_FETCHED",
+    "result":[
+      {
+        "description":"PROD",
+        "packId":"1",
+        "packName":"SOAP",
+        "docFileName":"doc.html",
+        "dataFileName":"data.html",
+        "usagePlan":[
+          {
+            "planId":"1",
+            "planName":"Gold",
+            "subscribed":true,
+            "subscriptionStatus":"active",
+            "subscriptionDuration":"6Months"
+          },
+          {
+            "planId":"2",
+            "planName":"Platinum",
+            "subscribed":false,
+            "subscriptionStatus":null,
+            "subscriptionDuration":"1Year"
+          },
+          {
+            "planId":"3",
+            "planName":"Silver",
+            "subscribed":true,
+            "subscriptionStatus":"active",
+            "subscriptionDuration":"3Months"
+          },
+          {
+            "planId":"4",
+            "planName":"Free",
+            "subscribed":false,
+            "subscriptionStatus":null,
+            "subscriptionDuration":"1Month"
+          }
+        ]
+      }
+    ],
+    "resultString":"",
+    "status":"SUCCESS",
+    "statusCode":200
+  }
+}
