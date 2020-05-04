681bf1d0a516e0c5758234206183cf8b4ace9521
Aksh2209 <akshayap672@gmail.com>
2020-05-04 22:18:19 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { AngularMaterial } from '@angular/material';
 
+import { AngularMaterial } from './angular.material';
 import { appRoutes } from './app.route';
 
 import { AppComponent } from './app.component';
